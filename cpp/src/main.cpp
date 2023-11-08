8389d00e1f5c1548ec5897aa8f1b4a40fa03849e
Michał Żelazko <148492076+mzelazko@users.noreply.github.com>
2023-11-08 21:49:20 +0100

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,16 +1,17 @@
-﻿#include "BitmapImage.h"
+#include "BitmapImage.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    BitmapImage bmpImage("image-01.bmp");
+    BitmapImage bmpImage("image-03.bmp");
     cout << "w: " << bmpImage.width << ", h: " << bmpImage.height << endl;
 
-    Pixel pixel = bmpImage.pixels[2 * bmpImage.width + 123];
+    Pixel pixel = bmpImage.pixels[1131 * bmpImage.width + 1131];
     cout << "Red: " << static_cast<int>(pixel.red)
         << ", Green: " << static_cast<int>(pixel.green)
-        << ", Blue: " << static_cast<int>(pixel.blue) << std::endl;
+        << ", Blue: " << static_cast<int>(pixel.blue) 
+        << ", alpha: " << static_cast<int>(pixel.alpha) << endl;
    
 
     return 0;

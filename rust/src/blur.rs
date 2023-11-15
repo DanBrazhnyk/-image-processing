296b8d8f43048cdbac0ab80eb0b60446241679ac
Filip Krawczyk <fil.krawczyk@gmail.com>
2023-11-15 22:16:33 +0100

    add box blur using open cv

--- rust/src/blur.rs
+++ rust/src/blur.rs
@@ -1,3 +1,25 @@
-pub fn blur(input_file: &String, output_file: &String, blur_amount: usize) {
-    //
+use opencv::{core, imgproc};
+
+pub fn opencv_box_blur(
+    input_file: &str,
+    output_file: &str,
+    blur_factor: i32,
+) -> opencv::Result<()> {
+    let img = opencv::imgcodecs::imread(input_file, opencv::imgcodecs::IMREAD_COLOR)?;
+
+    let mut blurred = core::Mat::default();
+
+    imgproc::blur(
+        &img,
+        &mut blurred,
+        core::Size::new(blur_factor, blur_factor),
+        core::Point::new(-1, -1),
+        core::BORDER_DEFAULT,
+    )?;
+
+    let params = opencv::core::Vector::new();
+    opencv::imgcodecs::imwrite(output_file, &blurred, &params)?;
+
+    Ok(())
+}
 }

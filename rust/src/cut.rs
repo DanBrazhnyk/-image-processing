bf73649c74c2d154b1215a54bb356c2bc90352f7
Filip Krawczyk <fil.krawczyk@gmail.com>
2024-01-14 00:42:37 +0100

    add cutting a shape in opencv

--- rust/src/cut.rs
+++ rust/src/cut.rs
@@ -1,13 +1,22 @@
-pub struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
-    // a: u8, // maybe?
-}
+use opencv::{
+    core::Point,
+    imgproc::{fill_convex_poly, LINE_8},
+};
 
-pub struct Point {
-    x: usize,
-    y: usize,
-}
+pub fn cut(
+    input_file: &str,
+    output_file: &str,
+    color: opencv::core::Scalar,
+    coordinates: Vec<Point>,
+) -> std::result::Result<(), opencv::Error> {
+    let mut input: opencv::prelude::Mat =
+        opencv::imgcodecs::imread(input_file, opencv::imgcodecs::IMREAD_COLOR)?;
 
-pub fn cut(image: &String, color: Color, coordinates: Vec<Point>) {}
+    let coords_vec: opencv::core::Vector<Point> = opencv::core::Vector::from(coordinates);
+    fill_convex_poly(&mut input, &coords_vec, color, LINE_8, 0)?;
+
+    let params = opencv::core::Vector::new();
+    opencv::imgcodecs::imwrite(output_file, &input, &params)?;
+
+    Ok(())
+}
